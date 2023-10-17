2eba5d8c705b517cd8443adc4a8ab4d5a9638440
Gabriel "Gabs <88252250+nijoko@users.noreply.github.com>
2023-10-17 07:52:29 -0300

    Update FaceDetectionTry1.py

--- FaceDetectionTry1.py
+++ FaceDetectionTry1.py
@@ -1,6 +1,7 @@
 from cvzone.FaceDetectionModule import FaceDetector
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 test = cv2.imread('images/image3.jpg')
 detector = FaceDetector()
@@ -9,10 +10,19 @@ if bboxs:
     x1, y1, w1, h1 = bboxs[0]['bbox']
     x2 = x1 + w1
     y2 = y1 + h1
-    img2 = cv2.imread("images/image4.jpg")
-    img2_resized = cv2.resize(img2, (w1, h1))
-    img[y1:y2, x1:x2] = img2_resized  # Inserta la imagen 2 en la imagen 1
+
+    # Aplicar el filtro sepia al área del rostro
+    img_face = img[y1:y2, x1:x2]
+    sepia_filter = np.array([[0.272, 0.534, 0.131],
+                             [0.349, 0.686, 0.168],
+                             [0.393, 0.769, 0.189]])
+    sepia_img = cv2.transform(img_face, sepia_filter)
+    sepia_img = np.clip(sepia_img, 0, 255)
+
+    img[y1:y2, x1:x2] = sepia_img  # Inserta la imagen con filtro sepia en la imagen original
+
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     plt.imshow(img)
     plt.axis('off')
     plt.show()
+
